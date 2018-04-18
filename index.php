b71bd9402a133a565437d3a824e68bea6c230504
JonaDelgado <jonathan.delgado25@gmail.com>
2018-04-18 02:17:01 -0300

    Archivo prueba
    Se sube archivo base - solo tiene codigo de prueba
    
    Co-Authored-By: lightoluft <lightoluft@users.noreply.github.com>

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Luft Soluciones Integrales</title>
+	<meta http-equiv="Content-type" content="text/html" charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="stylesheet" href="/vendor/css/bootstrap.css"/>
+  <link rel="stylesheet" href="/vendor/css/modern-business.css"/>
+
+  <script type="text/javascript" src="/vendor/js/jquery-3.3.1.js"></script>
+  <script type="text/javascript" src="/vendor/js/jqueryui/jquery-ui.js"></script>
+    <script type="text/javascript" src="/vendor/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="/vendor/validator/js/bootstrapValidator.js"></script>
+</head>
+<body>
+	<tabble>
+		<thead>
+			<tr>
+			<th>hola</th>
+			<th>prueba</th>
+			<th>1</th>
+			</tr>
+		</thead>
+
+		<tbody>
+			<tr>
+				<td>mundo</td>
+				<td>test</td>
+				<td>2</td>
+			</tr>
+		</tbody>
+	</tabble>
+</body>
+</html>
