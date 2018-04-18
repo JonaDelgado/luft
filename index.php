c1d1f70e401636f5f48b19083a1ae591249e8fc2
JonaDelgado <jonathan.delgado25@gmail.com>
2018-04-18 02:48:21 -0300

    Remplazo rutas
    Remplazo rutas incorrectas, boostrap ok

--- index.php
+++ index.php
@@ -5,21 +5,28 @@
 	<meta http-equiv="Content-type" content="text/html" charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <link rel="stylesheet" href="/vendor/css/bootstrap.css"/>
-  <link rel="stylesheet" href="/vendor/css/modern-business.css"/>
+      <link rel="stylesheet" href="vendor/css/bootstrap.css"/>
+      <link rel="stylesheet" href="vendor/css/modern-business.css"/>
+      <link rel="stylesheet" href="vendor/js/jqueryui/jquery-ui.css"/>
 
-  <script type="text/javascript" src="/vendor/js/jquery-3.3.1.js"></script>
-  <script type="text/javascript" src="/vendor/js/jqueryui/jquery-ui.js"></script>
-    <script type="text/javascript" src="/vendor/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="/vendor/validator/js/bootstrapValidator.js"></script>
+      <script type="text/javascript" src="vendor/js/jquery-3.3.1.js"></script>
+
+      <script type="text/javascript" src="vendor/js/jqueryui/jquery-ui.js"></script>
+
+      <script type="text/javascript" src="vendor/js/bootstrap.min.js"></script>
+      <script type="text/javascript" src="vendor/validator/js/bootstrapValidator.js"></script>
 </head>
 <body>
-	<tabble>
+
+	
+	<div class="container-fluid col-xs-10">
+	<table class="table table-bordered table-hover">
 		<thead>
 			<tr>
 			<th>hola</th>
 			<th>prueba</th>
 			<th>1</th>
+			<th>Boton Azul bootstrap OK</th>
 			</tr>
 		</thead>
 
@@ -28,8 +35,10 @@
 				<td>mundo</td>
 				<td>test</td>
 				<td>2</td>
-			</tr>
+				<td><button class="btn btn-primary">Boton Prueba</button> </td>
+			</tr> 
 		</tbody>
-	</tabble>
+	</table>
+	</div>
 </body>
 </html>
