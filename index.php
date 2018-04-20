a1e3f2ba0f63a2df9d9b258719ce776805b090bb
JonaDelgado <jonathan.delgado25@gmail.com>
2018-04-20 03:58:30 -0300

    Creacion de headers, footer y titulos
    Se crean las cabeceras, los titulos y el footer que llevara la web.
    
    Co-Authored-By: lightoluft <lightoluft@users.noreply.github.com>

--- index.php
+++ index.php
@@ -2,10 +2,11 @@
 <html lang="en">
 <head>
 	<title>Luft Soluciones Integrales</title>
+	<link rel="icon" href="favicon.png" type="image/png"> <!-- definir favicon (logo) -->
 	<meta http-equiv="Content-type" content="text/html" charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-      <link rel="stylesheet" href="vendor/css/bootstrap.css"/>
+      <link rel="stylesheet" href="vendor/css/bootstrap.css"/>         <!-- css bootstraps -->
       <link rel="stylesheet" href="vendor/css/modern-business.css"/>
       <link rel="stylesheet" href="vendor/js/jqueryui/jquery-ui.css"/>
 
@@ -15,30 +16,81 @@
 
       <script type="text/javascript" src="vendor/js/bootstrap.min.js"></script>
       <script type="text/javascript" src="vendor/validator/js/bootstrapValidator.js"></script>
+
+      <!--script type="text/javascript"> 
+  $(function () {
+    $("#boton").click(function () { 
+   alert("funciona");
+    
+    $("#boton").finish();
+   })
+  })
+
+  </script-->
+
+  <style>
+  	.footer{
+  		background-color: black;
+  		color: white;
+  		padding: 15px;
+  	}
+  </style>
 </head>
 <body>
 
+<header class="card-header" id="header">
 	
-	<div class="container-fluid col-xs-10">
-	<table class="table table-bordered table-hover">
-		<thead>
-			<tr>
-			<th>hola</th>
-			<th>prueba</th>
-			<th>1</th>
-			<th>Boton Azul bootstrap OK</th>
-			</tr>
-		</thead>
+</header>
 
-		<tbody>
-			<tr>
-				<td>mundo</td>
-				<td>test</td>
-				<td>2</td>
-				<td><button class="btn btn-primary">Boton Prueba</button> </td>
-			</tr> 
-		</tbody>
-	</table>
+<nav class="navbar navbar-inverse" id="nav"> <!-- barra de navegación -->
+
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<a class="navbar-brand" href="#">Luft</a>
+		</div>
+		<ul class="nav navbar-nav">
+			<li class="active"><a href="#">Home</a></li>
+			<li><a href="#historia">Nuestra Historia</a></li>
+			<li><a href="#servicios">Nuestro Servicios</a></li>
+			<li><a href="#nosotros">Quienes Somos</a></li>
+			<li><a href="#clientes">Nuestros Clientes</a></li>
+			<li><a href="#contacto">Contactanos</a></li>
+		</ul>
 	</div>
+
+</nav>  <!-- Fin Barra de navegación -->
+
+
+<div id="historia"> <h2 class="text-center">Historia</h2> <!-- Historia de la Empresa -->
+
+</div> <!-- Fin Historia -->
+
+
+<div id="servicios"><h2 class="text-center">Nuestros Servicios</h2> <!-- Servicios -->
+
+</div> <!--Fin Servicios-->
+
+
+<div id="nosotros"><h2 class="text-center">Quienes Somos</h2> <!--Quienes Somos -->
+
+</div> <!--Fin Quienes Somos-->
+
+
+<div id="clientes"><h2 class="text-center">Nuestros Clientes</h2> <!--Clientes-->
+
+</div> <!--Fin Clientes-->
+
+
+<div id="contacto"><h2 class="text-center">Contactanos</h2> <!--formulario Contacto -->
+
+</div><!--Fin formulario contacto-->
+
+<footer class="footer" id="footer"> <h5 class="text-center">LUFT Soluciones Integrales &copy </h5> <!-- footer-->
+	
+</footer> <!--Fin Footer-->
+
+	
+	
+	
 </body>
 </html>
